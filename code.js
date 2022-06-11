ff984b115360b4e2ec406e5db24cd881d2ca8dc5
subham agrawal <subhamagrwl@gmail.com>
2022-06-11 18:32:38 +0530

    first commit

---
+++ code.js
@@ -0,0 +1,12 @@
+document.getElementById("button").addEventListener("click", x);
+
+function x(e){
+
+    e.preventDefault();
+    console.log("click me");
+}
+window.addEventListener("DOMContentLoaded",Z);
+
+function Z(e){
+console.log('dom has loaded');
+}
